663628af313110fbc7443a3adce5c3abe8b5772f
maalbiero <mathieu.albiero@etu.uca.fr>
2022-05-17 16:21:06 +0200

    Ajout de getter et de setter pour certains des attributs de User, ajout d'un protocole d'égalité pour la class Car

--- twibook-app/src/model/Car.ts
+++ twibook-app/src/model/Car.ts
@@ -1,8 +1,8 @@
 class Car {
   readonly id: string;
-  private model: string;
-  private brand: string;
-  private color: Color;
+  private _model: string;
+  private _brand: string;
+  private _color: Color;
 
   public constructor(id: string, model: string,
     brand: string,
@@ -11,8 +11,16 @@ class Car {
       validateNewCar(id, model, brand);
 
       this.id = id;
-      this.model = model;
-      this.brand = brand;
-      this.color = color;
+      this._model = model;
+      this._brand = brand;
+      this._color = color;
+  }
+
+  public isEqual(carToCompare: Car){
+    if(this._model == carToCompare._model
+      && this._brand == carToCompare._brand
+      && this._color == carToCompare._color)
+      return true;
+    return false;
   }
 }
--- twibook-app/src/model/User.ts
+++ twibook-app/src/model/User.ts
@@ -1,11 +1,11 @@
 class User {
   readonly id: string;
-  public cars: Array<string>
-  public firstName: string;
-  public lastName: string;
-  public nickName: string;
-  public imageUrl: string;
-  public birthDate: Date;
+  private _cars: Array<Car>
+  private _firstName: string;
+  private _lastName: string;
+  private _nickName: string;
+  private _imageUrl: string;
+  private _birthDate: Date;
   readonly email: string;
   readonly hashedPwd: string;
 
@@ -21,13 +21,76 @@ class User {
      validateNewUser(id, firstName, lastName, nickName, imageUrl, email, hashedPwd);
 
      this.id = id;
-     this.cars = [];
-     this.firstName = firstName;
-     this.lastName = lastName;
-     this.nickName = nickName;
-     this.imageUrl = imageUrl;
-     this.birthDate = birthDate;
+     this._cars = [];
+     this._firstName = firstName;
+     this._lastName = lastName;
+     this._nickName = nickName;
+     this._imageUrl = imageUrl;
+     this._birthDate = birthDate;
      this.email = email;
      this.hashedPwd = hashedPwd;
  }
+
+  public get firstName(){
+    return this._firstName;
+  }
+
+  public set firstName(theFirstName: string){
+    if(this._firstName == null || this._firstName == "" || this._firstName.length > 50){
+      throw new Error("-firstName- est invalide");
+    }
+    this._firstName = theFirstName;
+  }
+
+  public get lastname(){
+  return this._lastName;
+  }
+
+  public set lastName(theLastName: string){
+    if(this._lastName == null || this._lastName == "" || this._lastName.length > 50){
+      throw new Error("-LastName- est invalide");
+    }
+    this._lastName = theLastName;
+  }
+
+  public get nickName(){
+    return this._nickName;
+  }
+
+  public set nickName(theNickName: string){
+    if(this._nickName == null || this._nickName == "" || this._nickName.length > 50){
+      throw new Error("-nickName- est invalide");
+    }
+    this._nickName = theNickName;
+  }
+
+  public get imageUrl(){
+    return this._imageUrl;
+  }
+
+  public set imageUrl(theImageUrl: string){
+    if(this._imageUrl == null || this._imageUrl == "" || this._imageUrl.length > 50){
+      throw new Error("-imageUrl- est invalide");
+    }
+    this._imageUrl = theImageUrl;
+  }
+
+  public get birthDate(){
+    return this._birthDate;
+  }
+
+  public set birthDate(theBirthDate: Date){
+    this._birthDate = theBirthDate;
+  }
+
+  public addCars(carToAdd: Car){
+    this._cars.push(carToAdd);
+  }
+
+  public removeCar(carToRemove: Car){
+    this._cars.forEach((element, index)=> {
+      if(element.isEqual(carToRemove)) this._cars.splice(index, 1);
+    });
+  }
+
 }
