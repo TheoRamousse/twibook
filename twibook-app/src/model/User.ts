9e49faa947acf9fa7132f1e3aac93d363fbbe606
Théo Ramousse <theo.ramousse@etu.uca.fr>
2022-05-22 10:14:05 +0200

    Finished model dor typescript

--- twibook-app/src/model/User.ts
+++ twibook-app/src/model/User.ts
@@ -1,13 +1,14 @@
 class User {
-  readonly id: string;
-  private _cars: Array<Car>
+  private readonly _id: string;
+  private readonly _cars: Array<Car>
   private _firstName: string;
   private _lastName: string;
   private _nickName: string;
   private _imageUrl: string;
   private _birthDate: Date;
-  readonly email: string;
-  readonly hashedPwd: string;
+  private readonly _email: string;
+  private readonly _hashedPwd: string;
+  private readonly _idPosts: Array<string>
 
   public constructor(id: string,
     firstName: string,
@@ -15,82 +16,103 @@ class User {
     nickName: string,
     imageUrl: string,
     birthDate: Date,
-   email: string,
-   hashedPwd: string) {
+    email: string,
+    hashedPwd: string) {
 
-     validateNewUser(id, firstName, lastName, nickName, imageUrl, email, hashedPwd);
+    validateNewUser(id, firstName, lastName, nickName, imageUrl, email, hashedPwd);
 
-     this.id = id;
-     this._cars = [];
-     this._firstName = firstName;
-     this._lastName = lastName;
-     this._nickName = nickName;
-     this._imageUrl = imageUrl;
-     this._birthDate = birthDate;
-     this.email = email;
-     this.hashedPwd = hashedPwd;
- }
+    this._id = id;
+    this._cars = [];
+    this._firstName = firstName;
+    this._lastName = lastName;
+    this._nickName = nickName;
+    this._imageUrl = imageUrl;
+    this._birthDate = birthDate;
+    this._email = email;
+    this._hashedPwd = hashedPwd;
+  }
 
-  public get firstName(){
+  public get firstName() {
     return this._firstName;
   }
 
-  public set firstName(theFirstName: string){
-    if (theFirstName == null || theFirstName == "" || theFirstName.length > 50){
+  public set firstName(theFirstName: string) {
+    if (theFirstName == null || theFirstName == "" || theFirstName.length > 50) {
       throw new Error("-firstName- est invalide");
     }
     this._firstName = theFirstName;
   }
 
-  public get lastname(){
-  return this._lastName;
+  public get lastname() {
+    return this._lastName;
   }
 
-  public set lastName(theLastName: string){
-    if (theLastName == null || theLastName == "" || theLastName.length > 50){
+  public set lastName(theLastName: string) {
+    if (theLastName == null || theLastName == "" || theLastName.length > 50) {
       throw new Error("-LastName- est invalide");
     }
     this._lastName = theLastName;
   }
 
-  public get nickName(){
+  public get nickName() {
     return this._nickName;
   }
 
-  public set nickName(theNickName: string){
-    if (theNickName == null || theNickName == "" || theNickName.length > 50){
+  public set nickName(theNickName: string) {
+    if (theNickName == null || theNickName == "" || theNickName.length > 50) {
       throw new Error("-nickName- est invalide");
     }
     this._nickName = theNickName;
   }
 
-  public get imageUrl(){
+  public get imageUrl() {
     return this._imageUrl;
   }
 
-  public set imageUrl(theImageUrl: string){
-    if (theImageUrl == null || theImageUrl == "" || theImageUrl.length > 50){
+  public set imageUrl(theImageUrl: string) {
+    if (theImageUrl == null || theImageUrl == "" || theImageUrl.length > 50) {
       throw new Error("-imageUrl- est invalide");
     }
     this._imageUrl = theImageUrl;
   }
 
-  public get birthDate(){
+  public get birthDate() {
     return this._birthDate;
   }
 
-  public set birthDate(theBirthDate: Date){
+  public set birthDate(theBirthDate: Date) {
     this._birthDate = theBirthDate;
   }
 
-  public addCar(carToAdd: Car){
+  public addCar(carToAdd: Car) {
     this._cars.push(carToAdd);
   }
 
-  public removeCar(carToRemove: Car){
-    this._cars.forEach((element, index)=> {
-      if(element.isEqual(carToRemove)) this._cars.splice(index, 1);
+  public removeCar(carToRemove: Car) {
+    this._cars.forEach((element, index) => {
+      if (element.isEqual(carToRemove)) this._cars.splice(index, 1);
     });
   }
+
+  public addPost(postId: String) {
+    this._cars.push(postId);
+  }
+
+  public removePost(postId: String) {
+    this._idPosts.forEach((element, index) => {
+      if (element == postId) this._idPosts.splice(index, 1);
+    });
+  }
+
+
+  public get email() {
+    return this._email;
+  }
+
+
+  public get hashedPassword() {
+    return this._hashedPwd;
+  }
+
 
 }
