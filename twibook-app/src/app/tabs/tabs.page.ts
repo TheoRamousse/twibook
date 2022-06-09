bbb57d495d0790a296642ef090a3e7f9e8774e8e
Mael BOUVARD <mbouvard@apsdigit.lan>
2022-06-09 15:26:55 +0200

    ADD - first impl of navbar

--- twibook-app/src/app/tabs/tabs.page.ts
+++ twibook-app/src/app/tabs/tabs.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AppControllerService } from '../services/app-controller.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-tabs',
