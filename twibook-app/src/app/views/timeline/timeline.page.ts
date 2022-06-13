924674906a5b68de083bab37c2e07271dd80f80a
Théo Ramousse <theo.ramousse@etu.uca.fr>
2022-06-13 11:56:26 +0200

    Tried to auto refresh

--- twibook-app/src/app/views/timeline/timeline.page.ts
+++ twibook-app/src/app/views/timeline/timeline.page.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, Directive, Inject } from '@angular/core';
 import { Post } from '../../model/Post'
 import { PersistenceTemplateService } from '../../services/persistence-template.service';
 import { StubService } from '../../services/stub.service';
+import { Router } from '@angular/router';
+import { AppControllerService } from '../../services/app-controller.service';
+import { WINDOW } from "../../services/window.service";
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-timeline',
@@ -11,17 +15,32 @@ import { StubService } from '../../services/stub.service';
     { provide: PersistenceTemplateService, useClass: StubService },
   ]
 })
+@Directive({ selector: '[trackScroll]' })
 export class TimelinePage {
   private posts: Array<Post> = []
   private NbPostsToLoad = 2
   private pageNumber = 1
-  private loadMoreVisibility = "visible"
 
-  constructor(private pers: PersistenceTemplateService) {
+  isLoading = false
+
+  constructor(private controlleur: AppControllerService, private router: Router, @Inject(WINDOW) private window: Window, @Inject(DOCUMENT) private document: Document) {
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    console.log('bottom of the page')
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight && !this.isLoading) {
+      console.log("bottom of the page");
+      this.pageNumber += 1;
+      this.isLoading = true;
+      this.onLoadNextPosts();
+      this.isLoading = false;
+    }
+  }
+
+
   private onLoadNextPosts() {
-    this.posts.push(...this.pers.getPostsPagined(this.pageNumber, this.NbPostsToLoad))
+    this.posts.push(...this.controlleur.getPostsPagined(this.pageNumber, this.NbPostsToLoad))
     this.pageNumber += 1
   }
 
