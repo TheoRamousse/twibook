62342382bb147f157da39c709d20304a8ee82a3f
maalbiero <mathieu.albiero@etu.uca.fr>
2022-06-14 22:31:24 +0200

    Ajout d'une recherche par nickName et en attente de confirmation pour le schema versionning pattern

--- twibook/src/main/java/iut/uca/twibook/repositories/CommentRepositoryV2.java
+++ twibook/src/main/java/iut/uca/twibook/repositories/CommentRepositoryV2.java
@@ -8,7 +8,7 @@ import org.springframework.data.mongodb.repository.Query;
 
 public interface CommentRepositoryV2 extends MongoRepository<CommentEntityV2,String> {
 
-    @Query ("{ 'schema_version' : '2' }")
+    @Query ("{ schema_version : '2', _id: ?0}")
     CommentEntityV2 findById(ObjectId id);
 
     Long removeById(ObjectId _id);
